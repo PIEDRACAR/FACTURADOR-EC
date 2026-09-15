a885595777f2afad68c6dbda73e59785ff441350
PIEDRACAR <crubenhp114@gmail.com>
2026-09-15 11:42:01 -0500

    Delete facturador-ec directory

--- facturador-ec/sql/migracion_firma_electronica_autoservicio_v997.sql
+++
@@ -1,7 +0,0 @@
--- CONTSERTRIB: configuración de firma electrónica por contribuyente
-alter table certificados add column if not exists p12_cifrado bytea;
-alter table certificados add column if not exists p12_password_cifrado bytea;
-alter table certificados add column if not exists fecha_expiracion date;
-alter table certificados add column if not exists activo boolean default true;
-alter table certificados add column if not exists alias text;
-create unique index if not exists certificados_emisor_alias_uq_997 on certificados(emisor_id, alias);
